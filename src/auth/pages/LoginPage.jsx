d0b177e47c2953251eb5ed5407506093b406221e
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-01-22 15:19:06 -0500

    Adding login/logout flow

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+
 export const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const onLogin = () => {
+        navigate('/', {
+            replace: true
+        });
+    };
+
     return(
-        <>
-            <h1>Login Page</h1>
-        </>
+        <div
+            className="container mt-5"
+        >
+            <h1>Login</h1>
+            <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={ onLogin }
+            >
+                Login
+            </button>
+
+        </div>
     );
 };
