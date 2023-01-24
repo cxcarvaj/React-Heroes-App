101f2fa7532600fe7a2cde88c36a476d15b13670
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-01-23 19:00:13 -0500

    Improvements in Search page

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -15,16 +15,17 @@ export const SearchPage = () => {
 
     const heroes = getHeroesByName( q );
 
+    const showSearch = q.length === 0;
+
+    const showError = q.length > 0 && heroes.length === 0;
+
     const { searchText, onInputChange } = useForm({
         searchText: q,
     });
 
     const onSearchSubmit = (event) => {
         event.preventDefault();
-        if( searchText.trim().length <= 1 ) return;
-
         navigate(`?q=${ searchText.toLowerCase().trim() }`);
-        
     };
 
 
@@ -58,11 +59,13 @@ export const SearchPage = () => {
                     <h4>Results</h4>
                     <hr />
 
-                    <div className="alert alert-primary">
+                    <div className="alert alert-primary animate__animated animate__fadeIn animate__fast" 
+                        style={{ display: showSearch ? '' : 'none' }}>
                         Search a hero
                     </div>
 
-                    <div className="alert alert-danger">
+                    <div className="alert alert-danger animate__animated animate__fadeIn animate__fast" 
+                        style={{ display: showError ? '' : 'none' }}>
                         No hero with <b>{ q }</b>
                     </div>
 
