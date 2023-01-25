c68f6567ce47e8898a3f3f7caabab631db42c15f
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-01-25 12:13:59 -0500

    Implementing private routes for not auth users

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
     return(
@@ -11,8 +12,12 @@ export const AppRouter = () => {
                 
                 <Route path="/login" element={ <LoginPage />}  />
 
-                <Route path="/*" element={ <HeroesRoutes />}  />
-
+                <Route path="/*" element={  
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                } />
+                
             </Routes>
         </>
     );
