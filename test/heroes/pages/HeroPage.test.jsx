11152665ca5e95694ff2014f381c25291b4f3f9d
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-02-14 17:42:09 -0500

    Add HeroPage unit testing

---
+++ test/heroes/pages/HeroPage.test.jsx
@@ -0,0 +1,57 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { HeroPage } from '../../../src/heroes';
+
+
+const mockedUseNavigate = jest.fn();
+const mockedUseParams = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'), 
+    useNavigate: () => mockedUseNavigate,
+    useParams: () => ({
+        id: 'marvel-iron'
+    }),
+}));
+
+
+describe('Test in <HeroPage />', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should show Iron Man info', () => {
+
+        render(
+            <MemoryRouter initialEntries={['/hero/marvel-iron']}>
+                <HeroPage />
+            </MemoryRouter>
+        );
+
+        expect( screen.getByText('Iron Man') ).toBeTruthy();
+
+        const img = screen.getByRole('img');
+
+        expect( img.src ).toContain('/assets/heroes/marvel-iron.jpg');
+
+    });
+
+
+    test('should go back when click on the Return button', () => {
+
+        render(
+            <MemoryRouter initialEntries={['/hero/marvel-iron']}>
+                <HeroPage />
+            </MemoryRouter>
+        );
+
+        const button = screen.getByRole('button');
+        
+        fireEvent.click(button);
+
+        expect( mockedUseNavigate ).toHaveBeenCalled();
+
+    });
+
+});
